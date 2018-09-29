613b33afce602702558f14cae6e9f84fe89a8797
John Snevily <snev7821@vandals.uidaho.edu>
2018-09-29 10:18:41 -0500

    added an array based decrementer, but logic to quit if the player increses the wrong health pool

---
+++ HealthDecrement5.cpp
@@ -0,0 +1,33 @@
+#include <iostream> //for write to console
+using namespace std; //TODO refactor to use local namespace
+/* 	Health Decrement5.cpp
+**	Use with CheatEngine to practice finding values.
+**	User has successfuly completed when they can increment health rather than decrease it
+**  However, there is a monster health value too, which we don't want to increase.
+**	V1.0.1 decrements an array of ints, it's your job to know which is the player.
+*/
+
+const int hero = 0;
+const int monster = 1;
+const int checksum = 2;
+
+int main() {
+  int health_array[3] = {100, 100, 100};
+  char inputstr[5];
+  //we'll do strings later, requires extra logic
+
+  while (true) {
+  health_array[hero]--;
+  health_array[monster]--;
+  health_array[checksum]--;
+  cout << "Ouch! " << health_array[hero] << " hero health left!" << endl;
+  cout << "Ouch! " << health_array[monster] << " monster health left!" << endl;
+  if(health_array[monster] > health_array[checksum]) { //player increased wrong health pool
+    cout << "Monster has grown too powerful!";
+    break;
+  }
+  cin >> inputstr;  //wait for input (no sanitization) but we're the attacker in this anyway, using cheatEngine
+  }
+    return 0;
+}
+
